c5d9037832383728faa57323230617a5ff5e1e0b
Truptimayee Ojha <128928239+TruptimayeeOjha@users.noreply.github.com>
2025-04-07 18:46:53 +0530

    Update mulDiseasePred.py

--- mulDiseasePred.py
+++ mulDiseasePred.py
@@ -1,85 +1,56 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Sun Jan 26 00:11:57 2025
-
-@author: trupt
-"""
-#  ## Diabetes:
-
-#         - Diabetes, also known as diabetes mellitus, is a chronic medical condition that occurs when the body is unable to properly regulate blood sugar levels. Blood sugar, or glucose, is the primary source of energy for the body's cells and tissues. However, in people with diabetes, the body cannot use glucose effectively, leading to high blood sugar levels.
-#         - Diabetes can lead to a variety of complications, including damage to the eyes, kidneys, nerves, and cardiovascular system. It is therefore important for people with diabetes to manage their blood sugar levels and receive regular medical check-ups to prevent or manage any complications.
-#         - Symptoms of diabetes include increased thirst and hunger, frequent urination, fatigue, blurred vision, and slow healing of cuts and bruises. However, some people with diabetes may not experience any symptoms at all, which is why regular screening is important, especially for individuals with risk factors such as a family history of diabetes, obesity, or a sedentary lifestyle.
-#         - In conclusion, diabetes is a chronic medical condition that affects the body's ability to regulate blood sugar levels. It can lead to a variety of complications, but with proper management and medical care, people with diabetes can live full and healthy lives.
-
-#         ## Heart Disease:
-
-#         - Heart disease, also known as cardiovascular disease, is a group of conditions that affect the heart and blood vessels. It is one of the leading causes of death worldwide and includes conditions such as coronary artery disease, heart failure, and arrhythmias.
-#         - Heart failure occurs when the heart is unable to pump blood effectively, leading to symptoms such as shortness of breath, fatigue, and swelling in the legs and ankles. Arrhythmias are abnormal heart rhythms that can cause the heart to beat too fast, too slow, or irregularly.
-#         - Symptoms of heart disease can vary depending on the type of condition but may include chest pain or discomfort, shortness of breath, fatigue, dizziness, nausea, and sweating. It is important to seek medical attention if you experience any of these symptoms.
-#         - In conclusion, heart disease is a group of conditions that affect the heart and blood vessels and can lead to serious complications. Lifestyle changes and medical care can help prevent and manage heart disease, but it is important to be aware of the symptoms and seek medical attention if necessary.
-        
-#         ## Parkinsons Disease:
-
-#         - Parkinson's disease is a progressive neurological disorder that affects movement. It occurs when the brain cells that produce the neurotransmitter dopamine are damaged or destroyed, leading to a decrease in dopamine levels. Dopamine is a chemical messenger that helps to control movement and coordination.
-#         - The symptoms of Parkinson's disease can vary from person to person but typically include tremors, stiffness, slowness of movement, and difficulty with balance and coordination. As the disease progresses, it can also cause cognitive and behavioral changes, including memory loss and depression.
-#         - While the cause of Parkinson's disease is not fully understood, it is believed to involve a combination of genetic and environmental factors. There is currently no cure for Parkinson's disease, but medications and therapies can help to manage symptoms and improve quality of life. These may include dopamine replacement therapy, physical therapy, and deep brain stimulation.
-#         - In conclusion, Parkinson's disease is a progressive neurological disorder that affects movement and coordination. While there is no cure for the disease, medications and therapies can help to manage symptoms and improve quality of life. Support and resources are available to help individuals and their families cope with the challenges of living with Parkinson's disease.
-#         """)
-
-
-# import os
-# import streamlit as st
-
-# try:
-#     from streamlit_option_menu import option_menu
-# except ImportError:
-#     os.system("pip install streamlit-option-menu")
-#     from streamlit_option_menu import option_menu
-
-from streamlit_option_menu import option_menu
 import pickle
 import streamlit as st
+from streamlit_option_menu import option_menu
+from PIL import Image
 
-# Sidebar navigation
-# st.sidebar.title("Navigation")
-# menu = ["Home", "Diabetes Prediction", "Heart Disease Prediction"]
-# choice = st.sidebar.radio("Go to", menu)
+# loading the saved models
+diabetes_model = pickle.load(open('diabetes_model.sav', 'rb'))
+heart_disease_model = pickle.load(open('heart_disease_model.sav', 'rb'))
+parkinsons_model = pickle.load(open('parkinsons_model.sav', 'rb'))
 
-# import subprocess
-# import sys
-
-# def install(package):
-#     subprocess.check_call([sys.executable, "-m", "pip", "install", package])
-
-# install("streamlit-option-menu==0.4.0")
-
-# from streamlit_option_menu import option_menu
-
-
-# Loading the saved models
-diabetes_model = pickle.load(open('diabetes_disease.sav','rb'))
-
-heart_disease_model = pickle.load(open('heart_disease_model.sav','rb'))
-
-parkinsons_model = pickle.load(open('parkinsons_disease.sav','rb'))
-
-# Sidebar for Navigation
+# sidebar for navigation
 with st.sidebar:
-    selected = option_menu(
-        'Multiple-Disease-Prediction-System', 
-        ['Diabetes Prediction', 'Heart Disease Prediction', 'Parkinsons Prediction'],
-        
-        icons = ['activity','heart','person'],
-        
-        default_index=1 #Default index shows the which page will open ,i.e,either diabetes or heart disease or parkinsons
-    )
+    selected = option_menu('Multiple Disease Prediction System',  
+                          ['Home','Diabetes Prediction',
+                           'Heart Disease Prediction',
+                           'Parkinsons Prediction','About'],
+                          icons=['house-fill','activity','heart','person','bi-info-circle-fill'],
+                          default_index=0)
 
 # Diabetes Prediction Page
-if selected == 'Diabetes Prediction':
-    # Page title
+if (selected == 'Home'):
+    st.title('Multiple Disease Prediction System')
+    image = Image.open("image.png")
+    st.image(image, use_column_width=True)
+    st.markdown("""
+        ## Diabetes:
+
+        - Diabetes, also known as diabetes mellitus, is a chronic medical condition that occurs when the body is unable to properly regulate blood sugar levels. Blood sugar, or glucose, is the primary source of energy for the body's cells and tissues. However, in people with diabetes, the body cannot use glucose effectively, leading to high blood sugar levels.
+        - Diabetes can lead to a variety of complications, including damage to the eyes, kidneys, nerves, and cardiovascular system. It is therefore important for people with diabetes to manage their blood sugar levels and receive regular medical check-ups to prevent or manage any complications.
+        - Symptoms of diabetes include increased thirst and hunger, frequent urination, fatigue, blurred vision, and slow healing of cuts and bruises. However, some people with diabetes may not experience any symptoms at all, which is why regular screening is important, especially for individuals with risk factors such as a family history of diabetes, obesity, or a sedentary lifestyle.
+        - In conclusion, diabetes is a chronic medical condition that affects the body's ability to regulate blood sugar levels. It can lead to a variety of complications, but with proper management and medical care, people with diabetes can live full and healthy lives.
+
+        ## Heart Disease:
+
+        - Heart disease, also known as cardiovascular disease, is a group of conditions that affect the heart and blood vessels. It is one of the leading causes of death worldwide and includes conditions such as coronary artery disease, heart failure, and arrhythmias.
+        - Heart failure occurs when the heart is unable to pump blood effectively, leading to symptoms such as shortness of breath, fatigue, and swelling in the legs and ankles. Arrhythmias are abnormal heart rhythms that can cause the heart to beat too fast, too slow, or irregularly.
+        - Symptoms of heart disease can vary depending on the type of condition but may include chest pain or discomfort, shortness of breath, fatigue, dizziness, nausea, and sweating. It is important to seek medical attention if you experience any of these symptoms.
+        - In conclusion, heart disease is a group of conditions that affect the heart and blood vessels and can lead to serious complications. Lifestyle changes and medical care can help prevent and manage heart disease, but it is important to be aware of the symptoms and seek medical attention if necessary.
+        
+        ## Parkinsons Disease:
+
+        - Parkinson's disease is a progressive neurological disorder that affects movement. It occurs when the brain cells that produce the neurotransmitter dopamine are damaged or destroyed, leading to a decrease in dopamine levels. Dopamine is a chemical messenger that helps to control movement and coordination.
+        - The symptoms of Parkinson's disease can vary from person to person but typically include tremors, stiffness, slowness of movement, and difficulty with balance and coordination. As the disease progresses, it can also cause cognitive and behavioral changes, including memory loss and depression.
+        - While the cause of Parkinson's disease is not fully understood, it is believed to involve a combination of genetic and environmental factors. There is currently no cure for Parkinson's disease, but medications and therapies can help to manage symptoms and improve quality of life. These may include dopamine replacement therapy, physical therapy, and deep brain stimulation.
+        - In conclusion, Parkinson's disease is a progressive neurological disorder that affects movement and coordination. While there is no cure for the disease, medications and therapies can help to manage symptoms and improve quality of life. Support and resources are available to help individuals and their families cope with the challenges of living with Parkinson's disease.
+        """)
+
+
+if (selected == 'Diabetes Prediction'):
+    # page title
     st.title('Diabetes Prediction using ML')
     
-    
+    # getting the input data from the user
     col1, col2, col3 = st.columns(3)
     with col1:
         Pregnancies = st.text_input('Number of Pregnancies')
@@ -98,24 +69,22 @@ if selected == 'Diabetes Prediction':
     with col2:
         Age = st.text_input('Age of the Person')
     # code for Prediction
-    diab_diagnosis = '' #stores the end result i.e a person is diabetic or non diabetic
+    diab_diagnosis = ''
     color =''
     # creating a button for Prediction
     if st.button('Diabetes Test Result'):
         diab_prediction = diabetes_model.predict([[Pregnancies, Glucose, BloodPressure, SkinThickness, Insulin, BMI, DiabetesPedigreeFunction, Age]])
-        #on the above ,all the inputs are given to the model and it predict whether the person is diabetic or not
-        if (diab_prediction[0] == 1): #diab_prediction[0] ,it means if the element in diab_prediction ==1
+        
+        if (diab_prediction[0] == 1):
           diab_diagnosis = 'The person is diabetic'
           color = 'Red'
         else:
           diab_diagnosis = 'The person is not diabetic'
           color = 'Green'
     st.markdown(f'<h3 style="color: {color};">{diab_diagnosis}</h3>', unsafe_allow_html=True)
-    
-    
-    
-    # Heart Disease Prediction Page
 
+
+# Heart Disease Prediction Page
 if (selected == 'Heart Disease Prediction'):
     # page title
     st.title('Heart Disease Prediction using ML')
@@ -258,5 +227,6 @@ if (selected == 'About'):
     - To make our project accessible and user-friendly, we deployed it using Streamlit, an open-source app framework used for building web applications. This deployment enables individuals to use our system with ease and access the predictions on the go.
     - In conclusion, our project is an innovative machine learning-based system designed to predict multiple diseases with high accuracy using various algorithms. With its deployment on Streamlit, individuals can access the system easily and use it to predict diseases, which can help them take necessary precautions and reduce the risks of contracting such diseases.
       """)
+st.write("<h5 style='color: pink;font-family: Arial;'>Developed By Truptimayee</h5>", unsafe_allow_html=True)
   
     
